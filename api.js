361679c0aa3aa574cb8cdbf618004acf9104e976
Sride Shankar <sridevishankar1161@gmail.com>
2024-07-05 11:40:36 +0530

    secure file

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const options = {
     method: 'POST',
     url: 'https://google-api31.p.rapidapi.com/imagesearch',
     headers: {
-      'x-rapidapi-key': 'c29e7e7ad7msh21556a6e22eb0e3p1feba6jsne3b200dfdfe2',
+      'x-rapidapi-key': '',
       'x-rapidapi-host': 'google-api31.p.rapidapi.com',
       'Content-Type': 'application/json'
     },
